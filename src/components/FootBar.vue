e51187bcca51abe9ed4b1b8574c76fe3512f979b
Calipride <layou1620@gmail.com>
2025-06-10 23:42:25 +0200

    commit final

---
+++ src/components/FootBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    
+      <footer>
+        <p>&copy; 2025 Cali's Camp. All rights reserved.</p>
+      </footer>
+    
+    </div>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style>
+  
+  footer {
+    background-color: #333;
+    color: #fff;
+    padding: 10px;
+    text-align: center;
+    margin-top: 20px;
+  }
+  
+  footer p {
+    margin: 0;
+  }
+  </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <NavBar />
+      <br>
+      <SearchForm />
+      <br>
+      <FootBar />
+      </div>
+    
+  </template>
+  
+  <script>
+  import NavBar from './NavBar.vue'
+  import SearchForm from './SearchForm.vue'
+  import FootBar from './FootBar.vue';
+  
+  export default {
+    components: {
+      
+      NavBar,
+      SearchForm,
+      FootBar
+      
+    },
+    
+  };
+  </script>
+  
+  <style scoped>
+  .user-page {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+    
+  }
+  </style>
